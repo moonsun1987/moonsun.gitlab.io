ff7eaa608b4c9a7ede338663d66093f6b356c2ec
Moon Sun <moonsun@firemail.cc>
2018-12-28 15:36:17 +0000

    run this brute force two sum

--- java/src/main/java/hello/HelloWorld.java
+++ java/src/main/java/hello/HelloWorld.java
@@ -6,8 +6,11 @@ public class HelloWorld {
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is: " + currentTime);
-
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
+    TwoSum twoSum = new TwoSum();
+    int[] result = twoSum.twoSum({2,7,11,15}, 9);
+    System.out.println(result[0]);
+    System.out.println(result[1]);
   }
 }
