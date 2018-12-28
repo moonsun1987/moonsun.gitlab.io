d5332675c23f2c4fac6dc99b783052188ff288a0
Moon Sun <moonsun@firemail.cc>
2018-12-28 16:01:07 +0000

    remove print statements
    I need to fix this error first 
    
    /builds/moonsun/moonsun.gitlab.io/java/src/main/java/hello/HelloWorld.java:12: 
    error: illegal start of expression
        int[] result = twoSum.twoSum({2,7,11,15}, 9);

--- java/src/main/java/hello/HelloWorld.java
+++ java/src/main/java/hello/HelloWorld.java
@@ -10,7 +10,5 @@ public class HelloWorld {
     System.out.println(greeter.sayHello());
     TwoSum twoSum = new TwoSum();
     int[] result = twoSum.twoSum({2,7,11,15}, 9);
-    System.out.println(result[0]);
-    System.out.println(result[1]);
   }
 }
